e30410814e5f6b25d25766dfd72bb6c0a1176b34
oyindas-hub <adebayoayomide789@gmail.com>
2024-12-20 14:11:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {navLinks.classList.toggle('show-nav');
+});
